246f9c4422d61ac981c10fe60b354a0bfd6a933b
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-04-05 19:08:26 -0500

    implementing the split_string function

--- src/coreutils.cpp
+++ src/coreutils.cpp
@@ -1,7 +1,21 @@
 #include "coreutils.h"
 
+using namespace std;
+
 void split_string()
 {
-	std::cout << "call to split_string()" << std::endl;
+	string s = "The cow is white";
+	string delimiter = " ";
+	string token;
+	size_t pos = 0;
+
+	while ((pos = s.find(delimiter)) != string::npos)
+	{
+		token = s.substr(0, pos);
+		cout << token << endl;
+		s.erase(0, pos + delimiter.length());
+	}
+
+	cout << s;
 }
 

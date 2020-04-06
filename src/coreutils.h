7eea4e8ced02aa1f07ce051db45d1f710f91b450
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-04-06 00:35:32 -0500

    improving the coreutils.h readability

--- src/coreutils.h
+++ src/coreutils.h
@@ -13,7 +13,9 @@
 namespace coreutils
 {
 	std::list<std::string> split_string(const std::string &);
+
 	void print_string_list(const std::list<std::string> &);
+
 	bool has_next(const std::list<std::string> &, const std::list<std::string>::const_iterator &);
 }
 

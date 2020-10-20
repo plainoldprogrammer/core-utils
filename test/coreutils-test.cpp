4267d22295f9ae8253983b7331a34c5df79dad37
plainoldprogrammer <plainoldprogrammer@gmail.com>
2020-10-20 12:42:06 -0500

    fixing the indentation

--- test/coreutils-test.cpp
+++ test/coreutils-test.cpp
@@ -10,26 +10,26 @@
 
 int main()
 {
-	system("cls");
+    system("cls");
 
-	std::string str = "This is a simple but complete and real example";
+    std::string str = "This is a simple but complete and real example";
 
-	std::cout << "str after its declaration: " << std::endl << str << std::endl << std::endl;
+    std::cout << "str after its declaration: " << std::endl << str << std::endl << std::endl;
 
-	std::cout << "call to coreutils::split_string(): " << std::endl;
-	std::list<std::string> splitted_str = coreutils::split_string(str);
-	std::cout << std::endl;
+    std::cout << "call to coreutils::split_string(): " << std::endl;
+    std::list<std::string> splitted_str = coreutils::split_string(str);
+    std::cout << std::endl;
 
-	std::cout << "call to coreutils::print_string_list(): " << std::endl;
-	coreutils::print_string_list(splitted_str);
-	std::cout << std::endl;
+    std::cout << "call to coreutils::print_string_list(): " << std::endl;
+    coreutils::print_string_list(splitted_str);
+    std::cout << std::endl;
 
-	std::cout << "call again to coreutils::print_string_list(): " << std::endl;
-	coreutils::print_string_list(splitted_str);
-	std::cout << std::endl;
+    std::cout << "call again to coreutils::print_string_list(): " << std::endl;
+    coreutils::print_string_list(splitted_str);
+    std::cout << std::endl;
 
-	std::cout << "str after the call to split_string(): " << std::endl << str << std::endl;
+    std::cout << "str after the call to split_string(): " << std::endl << str << std::endl;
 
-	return 0;
+    return 0;
 }
 
